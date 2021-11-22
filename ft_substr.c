e14340c021a77ec4b26f7125261129da421bf769
Rudyar <arthur.rudy.code@gmail.com>
2021-11-22 20:44:52 +0100

    substr done, add main.c for all mains

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 13:14:08 by arudy             #+#    #+#             */
-/*   Updated: 2021/11/22 15:22:40 by arudy            ###   ########.fr       */
+/*   Updated: 2021/11/22 20:43:40 by arudy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 static int	malloc_size(char const *s, unsigned int start, size_t len)
 {
 	int	size;
-	
+
 	size = 0;
 	while (s[start] && size <= (int)len)
 	{
@@ -27,26 +27,21 @@ static int	malloc_size(char const *s, unsigned int start, size_t len)
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	int		i;
-	int		size;
-	char	*dst;
+	unsigned int	i;
+	unsigned int	size;
+	char			*dst;
 
+	if (!s)
+		return (NULL);
 	size = malloc_size(s, start, len);
 	dst = malloc(sizeof(char) * (size + 1));
 	if (!dst)
 		return (NULL);
-	while (s[start] && i <= size)
+	if (start < size)
 	{
-		dst[i] = s[start];
-		start++;
-		i++;
+		while (s[start] && i < size)
+			dst[i++] = s[start++];
 	}
 	dst[i] = '\0';
 	return (dst);
-}
-
-int	main()
-{
-	printf("%s\n", ft_substr("Salut ca va ?", 4, 6));
-	return (0);
 }
---
+++ main.c
@@ -0,0 +1,6 @@
+//---- ft_substr -----
+// int	main(void)
+// {
+// 	printf("%s\n", ft_substr("Salut ca va ?", 4, 6));
+// 	return (0);
+// }
