377959733eec1322a545cd9177734892383cc7e2
arudy <arudy.student@42.fr>
2021-11-24 15:35:52 +0100

    strncmp done + simplify memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 14:16:13 by arudy             #+#    #+#             */
-/*   Updated: 2021/11/24 14:56:19 by arudy            ###   ########.fr       */
+/*   Updated: 2021/11/24 15:31:23 by arudy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,13 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t				i;
-	unsigned const char	*str1 = (unsigned const char*)s1;
-	unsigned const char	*str2 = (unsigned const char *)s2;
+	size_t	i;
 
 	i = 0;
 	while (i < n)
 	{
-		if (str1[i] != str2[i])
-		{
-			return (str1[i] - str2[i]);
-		}
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 		i++;
 	}
 	return (0);
